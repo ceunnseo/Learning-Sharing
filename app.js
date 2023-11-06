4594b80c709d1bfd3aeb00876519d60253b6fa23
ceunnseo <eunseo4068@gmail.com>
2023-11-06 19:49:58 +0900

    keyword 페이지와 모달창

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-$('a').click(function(event){
-    event.preventDefault(); 
+  const modal = document.querySelector('.modal');
+  const btnOpenModal=document.querySelector('.btn-open-modal');
+
+  btnOpenModal.addEventListener("click", ()=>{
+      modal.style.display="flex";
   });
+  modal.addEventListener("click", (e)=> {
+    if (e.target.className != 'modal_body') {
+      modal.style.display = "none";
+    }
+  });
+  
+
+/*
+  ('a').click(function(event){
+    event.preventDefault(); 
+  });*/
