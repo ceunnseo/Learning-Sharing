4dfe64d4e078ce88f2dce01e42ca9f34e852ae87
ceunnseo <eunseo4068@gmail.com>
2023-11-11 23:31:20 +0900

    모달창 변경

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
   const modal = document.querySelector('.modal');
   const btnOpenModal=document.querySelector('.btn-open-modal');
 
-  btnOpenModal.addEventListener("click", ()=>{
-      modal.style.display="flex";
-  });
+  const words = document.querySelector('.words');
+
+
   modal.addEventListener("click", (e)=> {
-    if (e.target.className != 'modal_body') {
+    console.log(e.target.className);
+    
+    if (e.target.className == 'modal') {
       modal.style.display = "none";
     }
   });
-  
+
+//word를 클릭하면 모달이 떠 있음
+  words.addEventListener("click", (e) => {
+    console.log("hi");
+    modal.style.display = "flex";
+  });
 
 /*
   ('a').click(function(event){
